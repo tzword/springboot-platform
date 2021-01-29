dd88f0279e004e42024b90e09ae0ccf0f44b6046
jianghy <tzword@163.com>
2021-01-29 20:39:42 +0800

    mq延时队列 修改日志

--- src/main/java/com/platform/common/config/RabbitConfig.java
+++ src/main/java/com/platform/common/config/RabbitConfig.java
@@ -39,7 +39,7 @@ public class RabbitConfig {
     @Bean
     public Queue orderQueue() {
         Map<String,Object> arguments = new HashMap<>(2);
-        // 绑定该队列到私信交换机
+        // 绑定该队列到死信交换机
         arguments.put("x-dead-letter-exchange",dlxExchange);
         arguments.put("x-dead-letter-routing-key",dlxRoutingKey);
         return new Queue(orderQueue,true,false,false,arguments);
@@ -67,7 +67,7 @@ public class RabbitConfig {
     }
 
     /**
-     * 声明死信队列
+     * 声明死信交换机
      * @return DirectExchange
      */
     @Bean
